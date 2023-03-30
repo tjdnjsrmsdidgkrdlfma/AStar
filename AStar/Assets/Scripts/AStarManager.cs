82b1ca6c8c6425bedede5ff2cf453d86507eccdb
tjdnjsrmsdidgkrID <tjdnjsrmsdidgkrdlapdlf@gmail.com>
2023-03-30 12:09:33 +0900

    핵심 작동 구조 구현

--- AStar/Assets/Scripts/AStarManager.cs
+++ AStar/Assets/Scripts/AStarManager.cs
@@ -28,6 +28,7 @@ public class AStarManager : MonoBehaviour
     Node start;
     Node end;
 
+    Vector2[] direction = new Vector2[4];
     Vector2 map_size = new Vector2(10, 18);
     #endregion
 
@@ -35,6 +36,9 @@ public class AStarManager : MonoBehaviour
     [Header("±‚≈∏")]
     [SerializeField] GameObject mouse_position;
     #endregion
+
+    List<Node> opened_list = new List<Node>();
+    List<Node> closed_list = new List<Node>();
 
     void Awake()
     {
@@ -46,6 +50,11 @@ public class AStarManager : MonoBehaviour
 
         start = null;
         end = null;
+
+        direction[0] = new Vector2(0, 1);
+        direction[1] = new Vector2(1, 0);
+        direction[2] = new Vector2(0, -1);
+        direction[3] = new Vector2(-1, 0);
     }
 
     void Start()
@@ -68,7 +77,7 @@ public class AStarManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            FindingPath();
+            StartCoroutine(FindingPath());
 
         if (Input.GetKeyDown(KeyCode.Alpha0))
             click_state = NodeState.None;
@@ -85,9 +94,121 @@ public class AStarManager : MonoBehaviour
         }
     }
 
-    void FindingPath()
+    IEnumerator FindingPath()
     {
+        opened_list.Add(start);
 
+        Vector2 node_index = GetIndex(start);
+
+        for (int i = 0; i < direction.Length; i++)
+        {
+            Node node = node_list[(int)node_index.y + (int)direction[i].y, (int)node_index.x + (int)direction[i].x];
+
+            if (node.State != NodeState.Wall)
+            {
+                opened_list.Add(node);
+                node.parent_node = start;
+                node.G = 1;
+                node.H = (int)Mathf.Abs(node.transform.position.x - end.transform.position.x)
+                         + (int)Mathf.Abs(node.transform.position.y - end.transform.position.y);
+                node.F = node.G + node.H;
+                node.GetComponent<SpriteRenderer>().color = Color.yellow;
+            }
+        }
+
+        opened_list.Remove(start);
+        closed_list.Add(start);
+
+        Node smallest_f_value_node;
+
+        while (true)
+        {
+            if (opened_list.Count == 0 || opened_list.Contains(end) == true)
+                break;
+
+            smallest_f_value_node = opened_list[0];
+
+            foreach (Node node in opened_list)
+            {
+                if (node.F < smallest_f_value_node.F)
+                {
+                    smallest_f_value_node = node;
+                }
+            }
+
+            yield return new WaitForSeconds(0.5f);
+
+            smallest_f_value_node.GetComponent<SpriteRenderer>().color = Color.green;
+            opened_list.Remove(smallest_f_value_node);
+            closed_list.Add(smallest_f_value_node);
+
+            node_index = GetIndex(smallest_f_value_node);
+
+            for (int i = 0; i < direction.Length; i++)
+            {
+                Node node = node_list[(int)node_index.y + (int)direction[i].y, (int)node_index.x + (int)direction[i].x];
+
+                if (node.State == NodeState.Wall
+                        || closed_list.Contains(node) == true)
+                    continue;
+
+                node.GetComponent<SpriteRenderer>().color = Color.yellow;
+                int temp_g = smallest_f_value_node.G + 1;
+                int temp_h = (int)Mathf.Abs(node.transform.position.x - end.transform.position.x)
+                             + (int)Mathf.Abs(node.transform.position.y - end.transform.position.y);
+                int temp_f = temp_g + temp_h;
+
+                if (opened_list.Contains(node) == true)
+                {
+                    if (node.G > temp_g)
+                    {
+                        node.parent_node = smallest_f_value_node;
+                        node.G = temp_g;
+                        node.H = temp_h;
+                        node.F = temp_f;
+                    }
+                }
+                else
+                {
+                    opened_list.Add(node);
+                    node.parent_node = smallest_f_value_node;
+                    node.G = temp_g;
+                    node.H = temp_h;
+                    node.F = temp_f;
+                }
+            }
+
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        //Stack<Node> node_stack = new Stack<Node>();
+        //Node temp_node = end;
+
+        //while (true)
+        //{
+        //    node_stack.Push(temp_node.parent_node);
+        //    temp_node = temp_node.parent_node;
+        //    if (temp_node == null)
+        //        break;
+        //}
+
+        //while (node_stack.Count > 0)
+        //{
+        //    temp_node = node_stack.Pop();
+        //    temp_node.GetComponent<SpriteRenderer>().color = Color.magenta;
+        //}
+
+        yield return null;
+    }
+
+    Vector2 GetIndex(Node node)
+    {
+        Vector2 position = node.transform.position;
+
+        position.x = (int)(position.x + (map_size.y / 2));
+        position.y = (int)(position.y + (map_size.x / 2));
+
+        return position;
     }
 
     void SetNodeState()
--- AStar/Assets/Scripts/Node.cs
+++ AStar/Assets/Scripts/Node.cs
@@ -1,14 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Node : MonoBehaviour
-{
-    public float g;
-    public float h;
-    public float f;
+{   
+    int g;
+    public int G
+    {
+        get { return g; }
+        set
+        {
+            g = value;
+            g_text.text = g.ToString();
+        }
+    }
+    int h;
+    public int H
+    {
+        get { return h; }
+        set
+        {
+            h = value;
+            h_text.text = h.ToString();
+        }
+    }
+    int f;
+    public int F
+    {
+        get { return f; }
+        set
+        {
+            f = value;
+            f_text.text = f.ToString();
+        }
+    }
 
     public Node parent_node;
+
+    [SerializeField] TextMeshPro g_text;
+    [SerializeField] TextMeshPro h_text;
+    [SerializeField] TextMeshPro f_text;
 
     AStarManager.NodeState state = AStarManager.NodeState.None;
     public AStarManager.NodeState State
@@ -30,7 +62,7 @@ public class Node : MonoBehaviour
                     sprite_renderer.color = Color.red;
                     break;
                 case AStarManager.NodeState.End:
-                    sprite_renderer.color = Color.blue; 
+                    sprite_renderer.color = Color.blue;
                     break;
             }
         }
@@ -40,6 +72,10 @@ public class Node : MonoBehaviour
 
     void Awake()
     {
+        g = 0;
+        h = 0;
+        f = 0;
+
         sprite_renderer = GetComponent<SpriteRenderer>();
     }
 }
